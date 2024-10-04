c9ab08cfe7dd27161efedd25d1766559d40ea2aa
Dryice Liu <dryiceliu@gmail.com>
2024-10-04 15:15:14 +0800

    open with webview

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { Browser } from '@capacitor/browser';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const openWebView = async () => {
+    await Browser.open({ url: 'https://msso1-uat.mercer.com/adfs/ls/idpinitiatedsignon.aspx?loginToRp=https%3A%2F%2Fshashi.skordev.com%2Fprofiles%2Fsso%2Fbya%2Fmetadata' });
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>WebView Demo</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen className="ion-padding">
+        <IonButton expand="block" onClick={openWebView}>Open WebView</IonButton>
       </IonContent>
     </IonPage>
   );
